36d33356da47f385c2fd748e747b4843254f00e5
Soichiro Yoshimura <yoshimura@soichiro.org>
2016-01-05 20:46:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 let http = require('http');
-var server = http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain',
 		'charset': 'utf-8'
